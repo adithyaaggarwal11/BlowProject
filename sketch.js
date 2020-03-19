42078a19171dbb4d388949bb5de1169f4638b8d3
Varsha Iyengar <varshaiyengar@hotmail.com>
2020-03-18 22:35:49 -0700

    Added tfJS model, constructRipple function

--- sketch.js
+++ sketch.js
@@ -1,49 +1,123 @@
-var phase = 0;
-var speed = 0.03;
-var maxSquareSize = 20;
-var numRows = 30;
+// animation variables
 var numCols = 30;
+var numRows = 30;
+var maxSquareSize = 20;
+var phase = 0;
+var col_c = 0; var row_r = 0;
+var y = 100;
+
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/r9YkmmWl/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
 
 function setup() {
-
-
-  // put setup code here
   createCanvas(1000, 1000);
-  noStroke();
-  // capture = createCapture(VIDEO);
-  // capture.size(500, 500);
-  colorA = color(255,255,255);
-  colorB = color(255, 255, 255);
+  video = createCapture(VIDEO);
+  video.size(100, 100);
+  video.hide();
 
-function draw() {
-  background(255);
-  // image(capture, 0, 0, 320, 240);
-  // filter(INVERT);
+  flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  // Create the video
+  classifyVideo();
 }
 
-}
 
 function draw() {
-  // put drawing code here
-  background(255,120,120);
-  // image(capture, 0, 0, 500, 500);
-  // filter(INVERT);
-  phase = frameCount*0.03;
+  background(255, 120, 120);
+  // Draw the video
+  image(flippedVideo, 0, 0);
 
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+
+
+  // as long as label == 1
+    // keep showing constructStaticSquares
+
+  // when label == 2
+    // start the ripple effect
+    // complete ripple effect (with no reference to labels)
+    // once complete launch constructStaticSquares
+
+  console.log(label)
+  // if (label == "Class 2") {
+  //
+  //   constructRipple(0.09);
+  // } else {
+
+  constructRipple(0.09);
+  // }
+}
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
+
+}
+
+// static square
+function constructSquares(){
   for(var col = 0; col<numCols; col+=1) {
     {
       for (var row = 0; row < numRows; row += 1) {
         var pos_y = height / 3.5 + row * 20;
         var pos_x = width / 3.5 + col * 20;
-        // fill(lerpColor(colorA, colorB, row/numRows))
-        var sizeOffset = sin(sin((phase))
+        square(pos_x, pos_y, 20);
+      }
+    }
+  }
+}
 
-        // var sizeOffset = (cos(phase - row * 10) + 1) * 0.5;
+// ripple animation
+function constructRipple(speed, squareSize){
+  redraw();
+  phase = frameCount*speed;
+  for(var col = 0; col<numCols; col+=1) {
+      for (var row = 0; row < numRows; row += 1) {
+        var pos_y = height / 3.5 + row * 20;
+        var pos_x = width / 3.5 + col * 20;
+        // // fill(lerpColor(colorA, colorB, row/numRows))
+        var sizeOffset = sin(col / (phase*row));
+        //
+        // // var sizeOffset = (cos(phase - row * 10) + 1) * 0.5;
         var squareSize = sizeOffset * maxSquareSize;
         square(pos_x, pos_y, squareSize);
       }
-
-    }
-
   }
+  squareSize = 20;
+}
+
+function resetSketch() {
 }
